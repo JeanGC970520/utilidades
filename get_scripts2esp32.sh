36e6cfc06b605bb9aed2ed676abe4553e111b1b2
Jean <jeanpoolgarcia_97@hotmail.com>
2022-10-28 18:38:38 -0500

    Funcionando al 100 ya con lo que se requiere descomentado

--- get_scripts2esp32.sh
+++ get_scripts2esp32.sh
@@ -8,13 +8,13 @@ function get_scripts {
 	for i in $lectura; do
 		if [[ $i =~ .+\.(py|json|mpy|log|md|txt)$ ]]; then #Para extraer los ficheros 
 			name=${i:${#dir}}
-			#ampy -p /dev/ttyUSB0 get ${i:1} ${i:1}
+			ampy -p /dev/ttyUSB0 get ${i:1} ${i:1}
 			echo ${i:1} ${i:1}
 		elif [[ ! $i =~ *\.* ]]; then #Para extraer lo que haya dentro de los directorios
 			echo "Dir: ${i:1}"
 			if [[ ! -e ${i:1} ]] ; then
 				echo "Generando directorio: ${i:1}"
-    		#mkdir ${i:1} # Genera el directorio
+    		mkdir ${i:1} # Genera el directorio
 			fi
 			subdir="${i:1}"
 			get_scripts
