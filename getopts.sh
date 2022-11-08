3fcc2db25168cf91b3f91adebd6e502e1a96bb91
Jean <jeanpoolgarcia_97@hotmail.com>
2022-11-08 13:57:54 -0600

    Explicando getopts, enlaces a teoria y ejemplos

--- getopts.sh
+++ getopts.sh
@@ -18,31 +18,34 @@ function exit_abnormal {
 
 ##   Las opciones con ':' reciben argumentos. 
 
-##   La variable OPTARG contiene la opcion que se paso al script (o comando bash).
+##   OPTARG Esta variable se establece en cualquier argumento para una opción encontrada por getopts. 
+##   También contiene el indicador de opción de una opción desconocida. https://wiki.bash-hackers.org/howto/getopts_tutorial
 
 ##   IMPORTANTE: Cuando se usaran opciones con ARGUMENTOS, colocar ':' (colon) antes de todos los nombres de las opciones: ":opt_1opt_2:opt_3..."
 #       'However, if you put a colon at the beginning of the optstring, getopts runs in "silent error checking mode."
 #       It will not report any verbose errors about options or arguments, and you need to perform error checking in your script.'
-#     Consultar: https://www.computerhope.com/unix/bash/getopts.htm
+#     Consultar: https://www.computerhope.com/unix/bash/getopts.htm https://www.shellscript.sh/tips/getopts/
 
 while getopts ":a:b:h" opt;
 do
-    case ${opt} in
+    case $opt in
         a)
-            echo "Pasaste la opcion -${OPTARG} al comando. Con los argumentos: $*"
+            shift $(( OPTIND - 2 ))   #Para posicionar el indice en el primer parametro despues de la opcion (a ó b, o la que sea)
+            echo "Pasaste la opcion -${opt} al comando. Con los argumentos: $*"
             ;;
         b)
-            echo "Pasaste la opcion -${OPTARG} al comando. Con los argumentos: $*"
+            shift $(( OPTIND - 2 ))  ## Con este comando sacamos el primer argumento que es la opcion elegida
+            echo "Pasaste la opcion -${opt} al comando. Con los argumentos: $*"
             ;;
         h)
             usage
             ;;
         :)
-            echo "Error: -${OPTARG} requiere argumentos"
+            echo "Error: -$OPTARG requiere argumentos"
             exit_abnormal
             ;;
-        *)
-            echo "Invalid option: $1." 
+        \?)
+            echo "Invalid option: -$OPTARG." 
             exit_abnormal
             ;;
 
