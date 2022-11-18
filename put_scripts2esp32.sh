d5f2f1091997e018d2c4a5dccaff080808b5e7f5
Jean <jeanpoolgarcia_97@hotmail.com>
2022-11-17 22:32:27 -0600

    Agregada barra de progreso

--- put_scripts2esp32.sh
+++ put_scripts2esp32.sh
@@ -15,14 +15,14 @@ function exit_abnormal {
 }
 
 function send_scripts {
-    #inicio=1
+    inicio=1
     num_scripts=$#
     echo "Se pasaran $num_scripts programas"
     for j in $*; do
-        echo "Escribiendo $j al esp32"
-        #ProgressBar $inicio $num_scripts
-        #inicio=$(( $inicio+1 ))
-        #sleep 0.2
+        ProgressBar $inicio $num_scripts
+        inicio=$(( $inicio+1 ))
+        #echo -ne "\n\rEscribiendo $j al esp32\n"
+        sleep 0.2
         ampy -p /dev/ttyUSB0 put $j
     done
     echo ""
@@ -31,9 +31,17 @@ function send_scripts {
 function send_all_directory {
     echo "Se leera todo el contenido del directorio y se le pasara al esp32"
     cont=$(ls -a $1)
+    rest=2          #Le resto dos por . y ..
+    if [[ $cont =~ main.py ]]; then rest=3; fi
+    total_objs=$(($(ls -a $1 | wc -l)-$rest))  #$(ls -a | wc -l) 
+    inicio=$(($total_objs-($total_objs-1)))
+    echo $total_objs $inicio
     for i in $cont; do
         if [[ $i != main.py && $i != "." && $i != ".." ]]; then 
-            echo "Escribiendo $i al esp32"
+            #echo "Escribiendo $i al esp32"
+            ProgressBar $inicio $total_objs
+            inicio=$(( $inicio+1 ))
+            sleep 0.2
             ampy -p /dev/ttyUSB0 put $i
         fi
     done
@@ -42,15 +50,15 @@ function send_all_directory {
 function ProgressBar {
 # Process data
     let _progress=(${1}*100/${2}*100)/100      
-    let _done=(${_progress}*80)/100            # Regla de tres, 80# son el 100%, s
-    let _left=80-$_done
+    let _done=(${_progress}*60)/100            # Regla de tres, 80# son el 100%, s
+    let _left=60-$_done
 # Build progressbar string lengths
     _fill=$(printf "%${_done}s")               # Genera el número de espacios vacios para lo que serán:  #
     _empty=$(printf "%${_left}s")              # Genera el número de espacios vacios para lo que serán:  -
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:                           
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"   # Sustitucion de espacios en blanco por # y de espacios en blanco por -
+printf "\rProgress : [${_fill// /\#}${_empty// /-}] $1 $2 ${_progress}%%"   # Sustitucion de espacios en blanco por # y de espacios en blanco por -
 
 }
 
