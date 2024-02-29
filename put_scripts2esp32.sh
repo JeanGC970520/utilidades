f67b9732384d2c084435260136f20b7e6eda668d
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-02-29 14:16:59 -0600

    Request a target port, fixing send directory feature and adding an third option that send all current directory content.

--- put_scripts2esp32.sh
+++ put_scripts2esp32.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script para cargarle todos los programas de un directorio
+#set -e
 
 COMAND=$0
 function usage {
@@ -7,6 +8,7 @@ function usage {
     echo "Writes N scripts using ampy command to sends its to esp32, options:"
     echo "      -s      Send only the SCRIPTS that are passed as parameters."
     echo "      -d      Send all DIRECTORY that are passed as parameter."
+    echo "      -a      Send all current DIRECTORY."
 }
 
 function exit_abnormal {
@@ -15,36 +17,40 @@ function exit_abnormal {
 }
 
 function send_scripts {
-    inicio=1
+    inicio=0
     num_scripts=$#
+    read -p "Proporciona por favor el puerto para hacer la conexion: " port
     echo "Se pasaran $num_scripts programas"
     for j in $*; do
         ProgressBar $inicio $num_scripts
         inicio=$(( $inicio+1 ))
         #echo -ne "\n\rEscribiendo $j al esp32\n"
         sleep 0.2
-        ampy -p /dev/ttyUSB0 put $j
+        ampy -p $port put $j
     done
+    ProgressBar $inicio $num_scripts
     echo ""
 }
 
 function send_all_directory {
-    echo "Se leera todo el contenido del directorio y se le pasara al esp32"
-    cont=$(ls -a $1)
+    read -p "Proporciona por favor el puerto para hacer la conexion: " port
+    echo "Se leera todo el contenido del directorio $pwd y se le pasara al esp32"
+    cont=$(ls -a $pwd)
     rest=2          #Le resto dos por . y ..
-    if [[ $cont =~ main.py ]]; then rest=3; fi
-    total_objs=$(($(ls -a $1 | wc -l)-$rest))  #$(ls -a | wc -l) 
-    inicio=$(($total_objs-($total_objs-1)))
-    echo $total_objs $inicio
+    if [[ $cont =~ ".git" ]]; then rest=3; fi
+    total_objs=$(($(ls -a $pwd | wc -l)-$rest))  #$(ls -a | wc -l) 
+    inicio=0
     for i in $cont; do
-        if [[ $i != main.py && $i != "." && $i != ".."  && $i != ".git" ]]; then 
+        if [[ ! $i =~ ^([.]+|.git)$ ]]; then 
             #echo "Escribiendo $i al esp32"
             ProgressBar $inicio $total_objs
             inicio=$(( $inicio+1 ))
             sleep 0.2
-            ampy -p /dev/ttyUSB0 put $i
+            ampy -p $port put $i
         fi
     done
+    ProgressBar $inicio $total_objs
+    echo ""
 }
 
 function ProgressBar {
@@ -58,22 +64,28 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:                           
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /\#}${_empty// /-}] $1 $2 ${_progress}%%"   # Sustitucion de espacios en blanco por # y de espacios en blanco por -
+printf "\rProgress : [${_fill// /▇}${_empty// /-}] $1 $2 ${_progress}%%"   # Sustitucion de espacios en blanco por # y de espacios en blanco por -
 
 }
 
-while getopts ":s:d:h" opt;
+while getopts ":s:d:ah" opt;
 do
     case ${opt} in
         s)
+            echo "Option to send some files"
             shift $(( OPTIND - 2 ))
             args=$*
             send_scripts $args 
             ;;
         d)
+            echo "Option to send a directory"
             shift $(( OPTIND - 2 ))
             args=$*
-            send_all_directory $args 
+            send_scripts $args 
+            ;;
+        a)
+            echo "Option to send all current directory"
+            send_all_directory
             ;;
         h)
             usage
